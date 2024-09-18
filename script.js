07640ce6af3c041a6aed465b78358c2812d84449
TrangPham916 <Trangpham916@gmail.com>
2024-09-18 21:36:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function calculateNumerology() {
 const name = document.getElementById('name').value.toUpperCase();
-    name = removeVietnameseTones(name);  // Chuyển tiếng Việt thành không dấu
   const dob = new Date(document.getElementById('dob').value);
   const day = dob.getDate();
   const month = dob.getMonth() + 1;
@@ -324,21 +323,3 @@ function getLessonDebt(name) {
   return missingNumbers.length > 0 ? missingNumbers.join(', ') : 'None';
 }
 
-function removeVietnameseTones(str) {
-            str = str.replace(/[\u0300-\u036f]/g, "");  // Xóa tổ hợp dấu Unicode
-            str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
-            str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
-            str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
-            str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
-            str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
-            str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
-            str = str.replace(/đ/g, "d");
-            str = str.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, "A");
-            str = str.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, "E");
-            str = str.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, "I");
-            str = str.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, "O");
-            str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, "U");
-            str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, "Y");
-            str = str.replace(/Đ/g, "D");
-            return str;
-        }
